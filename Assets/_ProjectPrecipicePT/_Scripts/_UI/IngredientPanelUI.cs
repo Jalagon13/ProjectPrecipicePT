ee45062fe7673ac0d38914ad9da06d6fe647f2b8
Jalagon <A1ag0nJus1in@gmail.com>
2026-04-21 13:09:39 +0900

    Crafting System underway

--- Assets/_ProjectPrecipicePT/_Scripts/_UI/IngredientPanelUI.cs
+++ Assets/_ProjectPrecipicePT/_Scripts/_UI/IngredientPanelUI.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,11 +9,40 @@ namespace ProjectPrecipicePT
     {
         [SerializeField] private Image _iconImage;
         [SerializeField] private TextMeshProUGUI _nameText;
-    
+        [SerializeField] private TextMeshProUGUI _amountNeededText;
+        [SerializeField] private Image _NotAvailableOverlay;
+        
+        private ItemRequirement _itemReq;
+        
+        private void Start()
+        {
+            InventoryManager.Instance.OnInventoryChanged += UpdateIngredientStatus;
+        }
+        
+        private void OnDestroy()
+        {
+            InventoryManager.Instance.OnInventoryChanged -= UpdateIngredientStatus;
+        }
+
+        private void UpdateIngredientStatus()
+        {
+            Debug.Log($"Ingredient Panel Updated");
+        }
+
         public void Setup(ItemRequirement itemRequirement)
         {
             _iconImage.sprite = itemRequirement.Item.InventoryIcon;
             _nameText.text = itemRequirement.Item.ItemName;
+            _amountNeededText.text = $"x{itemRequirement.Amount}";
+            _itemReq = itemRequirement;
+        }
+        
+        private bool HasRequiredIngredient()
+        {
+            // Loop through the inventory and check if I have enough of this ingredient or not.
+            // Create a function in the InventoryManager for this
+        
+            return true;
         }
     }
 }
